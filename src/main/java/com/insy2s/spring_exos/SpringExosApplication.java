51d6caaf6ad80ba3b359f50aa0dc0ddbbc1587b6
Pierre-Alexandre Crabbé <pa.ce.hepl@gmail.com>
2025-01-21 08:40:44 +0100

    First Commit

---
+++ src/main/java/com/insy2s/spring_exos/SpringExosApplication.java
@@ -0,0 +1,12 @@
+package com.insy2s.spring_exos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExosApplication.class, args);
+	}
+}
---
+++ src/test/java/com/insy2s/spring_firstproject/SpringExosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insy2s.spring_firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
